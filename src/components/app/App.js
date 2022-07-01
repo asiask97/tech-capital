9908338ed5e5eca47c180c89ed62881752c3a6d4
Asia <joannaskoc@gmail.com>
2022-07-01 11:02:13 +0100

    Inital commit - bolierplate for the webiste done and started working on navbar and hero section

---
+++ src/components/app/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import '../navbar/Navbar'
+import Navbar from '../navbar/Navbar';
+function App() {
+  return (
+    <div className="App">
+        <Navbar />
+        <h1>d</h1>
+        <h1>d</h1>
+        <h1>d</h1>
+        <h1>d</h1>
+        <h1>d</h1>
+        <h1>d</h1>
+        <h1>d</h1>
+        <h1>d</h1>
+        <h1>d</h1>        <h1>d</h1>        <h1>d</h1>        <h1>d</h1>        <h1>d</h1>        <h1>d</h1>        <h1>d</h1>        <h1>d</h1>
+        <h1>d</h1>
+        <h1>d</h1>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,44 @@
+
+import './Navbar.css';
+import { useState } from 'react';
+import logoWhite from '../../assets/images/logoLight.svg';
+import logoBlack from '../../assets/images/logoDark.svg';
+
+function Navbar() {
+  
+  // STOPPING ANIMATION FROM GOING OFF ON WINDOW RESIZE
+  let resizeTimer;
+  window.addEventListener("resize", () => {
+    document.getElementById('wrapper').classList.add("resize-animation-stopper");
+    
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+      document.getElementById('wrapper').classList.remove("resize-animation-stopper");
+    }, 400);
+  });
+
+  const [navbar, setNavbar] = useState(false);
+    
+  const controlNavBar = () => {
+      if(window.scrollY > 50){
+          setNavbar(true);
+      }else{
+          setNavbar(false);
+      }
+  }
+  window.addEventListener("scroll", controlNavBar);
+  
+  const handleClickNav = () =>{
+    document.getElementById('Hero').scrollIntoView( { behavior: "smooth" })
+  }
+
+  return (
+    <div className="Navbar">
+        <div id='wrapper' className={navbar ? 'activeNav wrapper' : 'wrapper'}>
+          <img className ='img' width ='20px' src={navbar ? logoBlack : logoWhite} alt='logo' onClick={handleClickNav}/>
+        </div>
+    </div>
+  );
+}
+
+export default Navbar;
